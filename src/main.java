ad6f2a242ebeda9e2d6ebc3483e5ab632812fb4a
tevuori <kuba.horak00754@gmail.com>
2022-04-24 20:41:21 +0200

    First Dice

--- src/main.java
+++ src/main.java
@@ -1,5 +1,21 @@
 public class main {
     public static void main(String[] args){
+        printDice(1);
+    }
+    public static void printDice(int number){
+        switch(number){
+            case 1:
+                String[] dice = new String[]{
+                        "* * * * *",
+                        "*       *",
+                        "*   #   *",
+                        "*       *",
+                        "* * * * *"
+                };
+                for (String s: dice) {System.out.println(s);}
+                break;
+                
+        }
 
     }
 }
