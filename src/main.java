b108cf7dd7235f103c1c1abb967b64cb94791b06
tevuori <kuba.horak00754@gmail.com>
2022-04-24 20:43:57 +0200

    More dices

--- src/main.java
+++ src/main.java
@@ -1,20 +1,68 @@
 public class main {
     public static void main(String[] args){
-        printDice(1);
     }
     public static void printDice(int number){
         switch(number){
             case 1:
-                String[] dice = new String[]{
+                String[] dice1 = new String[]{
                         "* * * * *",
                         "*       *",
                         "*   #   *",
                         "*       *",
                         "* * * * *"
                 };
-                for (String s: dice) {System.out.println(s);}
+                for (String s: dice1) {System.out.println(s);}
                 break;
-                
+            case 2:
+                String[] dice2 = new String[]{
+                        "* * * * *",
+                        "*    #  *",
+                        "*       *",
+                        "*  #    *",
+                        "* * * * *"
+                };
+                for (String s: dice2) {System.out.println(s);}
+                break;
+            case 3:
+                String[] dice3 = new String[]{
+                        "* * * * *",
+                        "*    #  *",
+                        "*   #   *",
+                        "*  #    *",
+                        "* * * * *"
+                };
+                for (String s: dice3) {System.out.println(s);}
+                break;
+            case 4:
+                String[] dice4 = new String[]{
+                        "* * * * *",
+                        "* #   # *",
+                        "*       *",
+                        "* #   # *",
+                        "* * * * *"
+                };
+                for (String s: dice4) {System.out.println(s);}
+                break;
+            case 5:
+                String[] dice5 = new String[]{
+                        "* * * * *",
+                        "* #   # *",
+                        "*   #   *",
+                        "* #   # *",
+                        "* * * * *"
+                };
+                for (String s: dice5) {System.out.println(s);}
+                break;
+            case 6:
+                String[] dice6 = new String[]{
+                        "* * * * *",
+                        "* #   # *",
+                        "* #   # *",
+                        "* #   # *",
+                        "* * * * *"
+                };
+                for (String s: dice6) {System.out.println(s);}
+                break;
         }
 
     }
